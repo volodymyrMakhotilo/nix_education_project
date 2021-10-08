c26600e9fb788f134cf96a169f5ef44bf82dcf55
volodymyrMakhotilo <vkmahotilo@gmail.com>
2021-10-08 18:01:04 +0300

    Added inserting data into db using concurrency.

--- main.go
+++ main.go
@@ -1,26 +1,86 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"nix_education_beginner_project/models"
 )
 
 const domain string = "https://jsonplaceholder.typicode.com"
+const userID int = 7
 
-func getPost(index int, c chan []byte) {
-	resp, err := http.Get(domain + fmt.Sprintf("/posts/%d", index))
+func AddComment(comment models.Comment) {
+	log.Println("Entered addComments")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/beginner")
+	_, _ = db.Query("INSERT  INTO `beginner`.`comments`(post_id,name,email,body)" + fmt.Sprintf("VALUES(%v,'%v','%v','%v') ", comment.PostId, comment.Name, comment.Email, comment.Body))
+	/*	defer rows.Close();*/
+	defer db.Close()
 	if err != nil {
 		panic(err)
 	}
-	body, err := io.ReadAll(resp.Body)
+}
+
+func getComments(index uint) {
+	log.Println("Entered getComments")
+
+	var comments []models.Comment
+
+	resp, err := http.Get(domain + fmt.Sprintf("/comments?postId=%d", index))
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &comments)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, comment := range comments {
+		go AddComment(comment)
+	}
+}
+
+func getPost(index int, c chan []byte) {
+	log.Println("Entered getPost")
+
+	var posts []models.Post
+
+	resp, err := http.Get(domain + fmt.Sprintf("/posts?userId=%d", index))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &posts)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, post := range posts {
+		go getComments(post.Id)
+	}
+
 	c <- body
 }
+
 func CreateFile(data []byte) {
 	file, err := ioutil.TempFile("./storage/posts", "post")
 	if err != nil {
@@ -30,9 +90,14 @@ func CreateFile(data []byte) {
 }
 
 func main() {
+	var err error
+	if err != nil {
+		panic(err)
+	}
 	c := make(chan []byte, 100)
 	for i := 1; i <= 100; i++ {
-		go getPost(i, c)
+		go getPost(userID, c)
+
 	}
 	for i := 1; i <= 100; i++ {
 		CreateFile(<-c)
