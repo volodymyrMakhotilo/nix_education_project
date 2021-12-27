c384e4004595691f463bc6ff0223fb2d16ae4689
volodymyrMakhotilo <vkmahotilo@gmail.com>
2021-12-27 15:40:26 +0200

    managed #4

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
-	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
+	golang.org/x/tools v0.1.8 // indirect
 )
--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	_ "golang.org/x/lint"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"nix_education_beginner_project/models"
+	"time"
 )
 
 const domain string = "https://jsonplaceholder.typicode.com"
@@ -17,45 +18,34 @@ const userID int = 7
 
 var db *sql.DB
 
-func AddComment(comment models.Comment) {
-	log.Println("Entered addComments")
-	db, _ = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/beginner")
-	_, err := db.Exec("INSERT  INTO `beginner`.`comments`(post_id,name,email,body)" + fmt.Sprintf("VALUES(%v,'%v','%v','%v') ", comment.PostId, comment.Name, comment.Email, comment.Body))
-	defer db.Close()
-	if err != nil {
-		panic(err)
-	}
+func addComment(comment models.Comment) {
+	db.Exec("INSERT  INTO `beginner`.`comments`(post_id,name,email,body)" + fmt.Sprintf("VALUES(%v,'%v','%v','%v') ", comment.PostId, comment.Name, comment.Email, comment.Body))
 }
 
 func getComments(index uint) {
-	log.Println("Entered getComments")
-
 	var comments []models.Comment
 
 	resp, err := http.Get(domain + fmt.Sprintf("/comments?postId=%d", index))
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-
+	defer resp.Body.Close()
 	err = json.Unmarshal(body, &comments)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, comment := range comments {
-		go AddComment(comment)
+		go addComment(comment)
 	}
 }
 
 func getPost(index int, c chan []byte) {
-	log.Println("Entered getPost")
-
 	var posts []models.Post
 
 	resp, err := http.Get(domain + fmt.Sprintf("/posts?userId=%d", index))
@@ -82,7 +72,7 @@ func getPost(index int, c chan []byte) {
 	c <- body
 }
 
-func CreateFile(data []byte) {
+func createFile(data []byte) {
 	file, err := ioutil.TempFile("./storage/posts", "post")
 	if err != nil {
 		panic(err)
@@ -92,15 +82,21 @@ func CreateFile(data []byte) {
 
 func main() {
 	var err error
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/beginner")
 	if err != nil {
 		panic(err)
 	}
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxLifetime(50 * time.Millisecond)
+	db.SetMaxIdleConns(100)
+
 	c := make(chan []byte, 100)
 	for i := 1; i <= 100; i++ {
 		go getPost(userID, c)
 
 	}
 	for i := 1; i <= 100; i++ {
-		CreateFile(<-c)
+		createFile(<-c)
 	}
+
 }
